66f670843e392efa5e8eba14be98379fedc821a3
Abel chartier <abel.chartier@epita.fr>
2025-02-21 20:46:37 +0100

    feat(plugins): adds a bunch of plugins

--- config/plugins/utils/snacks.nix
+++ config/plugins/utils/snacks.nix
@@ -1,18 +1,21 @@
 {
-  plugings.snacks = {
-    animate = {
-      enabled = true;
-    };
-    notifier = {
-      enabled = true;
-      timeout = 3000;
-    };
-    quickfile = {
-      enabled = false;
-    };
-    words = {
-      debounce = 100;
-      enabled = true;
+  plugins.snacks = {
+    enable = true;
+    settings = {
+      animate = {
+        enabled = true;
+      };
+      notifier = {
+        enabled = true;
+        timeout = 3000;
+      };
+      quickfile = {
+        enabled = false;
+      };
+      words = {
+        debounce = 100;
+        enabled = true;
+      };
     };
   };
 }
