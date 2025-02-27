6aee436abc30d8ae956c7ec9e21236367beb7102
Abel chartier <abel.chartier@epita.fr>
2025-02-27 14:43:20 +0100

    feat(clangd): adds clang-tidy to start cmd

--- config/default.nix
+++ config/default.nix
@@ -24,7 +24,6 @@ _: {
     ./plugins/git/gitsigns.nix
 
     ./plugins/lsp/conform.nix
-    ./plugins/lsp/cmake-tools.nix
     ./plugins/lsp/fidget.nix
     ./plugins/lsp/lsp.nix
 
--- config/plugins/lsp/lsp.nix
+++ config/plugins/lsp/lsp.nix
@@ -26,6 +26,26 @@
         };
         clangd = {
           enable = true;
+          extraOptions = {
+            capabilities = {
+              offsetEncoding = [ "utf-16" ];
+            };
+
+            init_options = {
+              usePlaceholders = true;
+              completeUnimported = true;
+              clangdFileStatus = true;
+            };
+          };
+          cmd = [
+            "clangd"
+            "--background-index"
+            "--clang-tidy"
+            "--header-insertion=iwyu"
+            "--completion-style=detailed"
+            "--function-arg-placeholders"
+            "--fallback-style=llvm"
+          ];
         };
         /*
           jdtls = {
