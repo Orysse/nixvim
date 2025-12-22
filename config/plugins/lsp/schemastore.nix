f43c6be5266b7dbc28e46b69ed09e2c32c4102cc
Abel Chartier <Abel.CHARTIER@cea.fr>
2025-12-22 13:35:26 +0100

    fix(shemastore): try desperate fixes

--- config/plugins/lsp/schemastore.nix
+++ config/plugins/lsp/schemastore.nix
@@ -5,35 +5,20 @@
   ...
 }:
 lib.mkIf config.plugins.lsp.servers.yamlls.enable {
-  plugins.yaml-schema-detect = {
-    enable = true;
-    settings = {
-      keymap = {
-        refresh = "<leader>yr";
-        info = "<leader>yi";
-        cleanup = "<leader>yc";
-      };
-    };
-  };
-
   plugins.schemastore = {
     enable = true;
     yaml = {
       enable = true;
       settings = {
-        extra = [
-          {
-            name = "GitLab CI";
-            description = "GitLab CI/CD pipeline configuration";
-            fileMatch = [
-              ".gitlab-ci/**/*.yaml"
-              ".gitlab-ci/**/*.yml"
-              "gitlab-ci/**/*.yaml"
-              "gitlab-ci/**/*.yml"
-            ];
-            url = "https://gitlab.com/gitlab-org/gitlab/-/raw/master/app/assets/javascripts/editor/schema/ci.json";
-          }
-        ];
+        schemas = {
+          "https://gitlab.com/gitlab-org/gitlab/-/raw/master/app/assets/javascripts/editor/schema/ci.json" = [
+            ".gitlab-ci.yml"
+            ".gitlab-ci/**/*.yml"
+            ".gitlab-ci/**/*.yaml"
+            "gitlab-ci/**/*.yml"
+            "gitlab-ci/**/*.yaml"
+          ];
+        };
       };
     };
   };
