1219696d2cc81f7a691b227d7a93b6c0e9f12d32
Abel Chartier <abel.chartier@gmail.com>
2026-03-06 12:32:40 +0100

    fix(plugins): disable format on save toggle now works

--- config/plugins/formatting/conform.nix
+++ config/plugins/formatting/conform.nix
@@ -3,77 +3,82 @@ _: {
     ./filetypes.nix
     ./formatters.nix
   ];
-  extraConfigLuaPre = ''
-    local slow_format_filetypes = {}
-
-    vim.api.nvim_create_user_command("FormatDisable", function(args)
-      if args.bang then
-        vim.b.disable_autoformat = true
-      else
-        vim.g.disable_autoformat = true
-      end
-    end, {
-      desc = "Disable autoformat-on-save",
-      bang = true,
-    })
-
-    vim.api.nvim_create_user_command("FormatEnable", function()
-      vim.b.disable_autoformat = false
-      vim.g.disable_autoformat = false
-    end, {
-      desc = "Re-enable autoformat-on-save",
-    })
-
-    vim.api.nvim_create_user_command("FormatToggle", function(args)
-      if args.bang then
-        vim.b.disable_autoformat = not vim.b.disable_autoformat
-      else
-        vim.g.disable_autoformat = not vim.g.disable_autoformat
-      end
-    end, {
-      desc = "Toggle autoformat-on-save",
-      bang = true,
-    })
-  '';
-
   plugins.conform-nvim = {
     enable = true;
     settings = {
       notify_on_error = true;
 
-      format_on_save = ''
+      format_on_save.__raw = ''
         function(bufnr)
           if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
             return
           end
-
-          if slow_format_filetypes[vim.bo[bufnr].filetype] then
-            return
-          end
-
-          local function on_format(err)
-            if err and err:match("timeout$") then
-              slow_format_filetypes[vim.bo[bufnr].filetype] = true
-            end
-          end
-
-          return { timeout_ms = 200, lsp_fallback = true }, on_format
-        end
-      '';
-
-      format_after_save = ''
-        function(bufnr)
-          if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
-            return
-          end
-
-          if not slow_format_filetypes[vim.bo[bufnr].filetype] then
-            return
-          end
-
-          return { lsp_fallback = true }
+          return { lsp_format = "fallback", timeout_ms = 500 }
         end
       '';
     };
   };
+  keymaps = [
+    {
+      mode = "n";
+      key = "<leader>tf";
+      action.__raw = ''
+        function()
+          vim.g.disable_autoformat = not vim.g.disable_autoformat
+          vim.notify(
+            vim.g.disable_autoformat and "Format on save disabled" or "Format on save enabled",
+            vim.log.levels.INFO
+          )
+        end
+      '';
+      options = {
+        desc = "Toggle format on save (global)";
+        silent = true;
+      };
+    }
+    {
+      mode = "n";
+      key = "<leader>tF";
+      action.__raw = ''
+        function()
+          vim.b.disable_autoformat = not vim.b.disable_autoformat
+          vim.notify(
+            vim.b.disable_autoformat and "Format on save disabled (buffer)" or "Format on save enabled (buffer)",
+            vim.log.levels.INFO
+          )
+        end
+      '';
+      options = {
+        desc = "Toggle format on save (buffer)";
+        silent = true;
+      };
+    }
+    {
+      mode = "n";
+      key = "<leader>cf";
+      action.__raw = ''
+        function()
+          require("conform").format({ async = true, lsp_format = "fallback" })
+        end
+      '';
+      options = {
+        desc = "Format buffer";
+        silent = true;
+      };
+    }
+    {
+      mode = "v";
+      key = "<leader>cf";
+      action.__raw = ''
+        function()
+          require("conform").format({ async = true, lsp_format = "fallback" })
+        end
+      '';
+      options = {
+        desc = "Format selection";
+        silent = true;
+      };
+    }
+  ];
+
 }
